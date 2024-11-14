d0522c2337328106ddf1f5c0dda482c4debb4f28
Sabbir's mac <mac@macs-MacBook-Air.local>
2024-11-15 01:22:47 +0600

    something in chats history

--- Frontend/src/home/right/Chatmessage.jsx
+++ Frontend/src/home/right/Chatmessage.jsx
@@ -18,11 +18,11 @@ function Get() {
         <>
           <div className="py-2">
             <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-info mx-3 max-w-[50%]">Calm down, Anakin.mx-3 max-w-[55%] mx-3 max-w-[55%] mx-3 max-w-[55%] mx-3 max-w-[55%]</div>
+              <div className="chat-bubble chat-bubble-info mx-3 max-w-[50%]">Calm down, Anakin.mx-3 max-w-[55%] mx-3 max-w-[55%] mx-3 max-w-[70%] mx-3 max-w-[55%]</div>
             </div>
           </div>
           <div className="chat chat-end">
-            <div className="chat-bubble chat-bubble-accent mx-3 max-w-[50%]">
+            <div className="chat-bubble chat-bubble-accent mx-3 max-w-[60%]">
               That's never been done in the history of the Jedi. It's insulting!
               It's insulting! It's insulting! It's insulting!It's insulting! It's insulting!
             </div>
