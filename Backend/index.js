db0cbb12b18bddb596fe99ff144281bafcdc1f92
Md. Sabbir Hossain <102421396+mdsabbir12@users.noreply.github.com>
2025-07-15 15:12:53 +0600

    push somthing

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoute from "./route/user.route.js";
 import messageRoute from "./route/message.route.js";
+import path from "path"
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import {app ,server} from "./Socketserver/server.js";
@@ -31,6 +32,20 @@ try{
 
 app.use("/api/user",userRoute);
 app.use("/api/message",messageRoute);
+
+
+
+
+// ----------------  Deployment Code -----------
+if(process.env.NODE_ENV === "production"){
+  const dirpath=path.resolve();
+  app.use(express.static("./Frontend/dist"));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(dirpath,'./Frontend/dist','index.html'))
+  })
+}
+
+
 
 server.listen(PORT, () => {
   console.log(`Server is Running on port ${PORT}`)
